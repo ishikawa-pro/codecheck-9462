eff755bac73eafceb1de5bc7e866d3ee7f41ae22
ishikawa-pro <ishikawa.pro@gmail.com>
2017-04-12 19:04:12 +0900

    [fix]パラメターの個数に応じてイテレートするようになっていた部分を削除

--- app/main.rb
+++ app/main.rb
@@ -7,26 +7,24 @@ def main(argv)
       if argv[0] == nil then
         raise "パラメータがセットされていません"
       end
-      argv.each do |v|
-          #パラメータをセット
-          #urlで"+"は" "になるので"%2b"にエスケープしてから代入
-          param = v.to_s.gsub("+","%2b") 
-          #urlを作成
-          uri = URI.parse('http://challenge-server.code-check.io/api/hash?q='+param)
-          
-          request = Net::HTTP::Get.new(uri.request_uri)
-          # Request headers
-          request['Content-Type'] = 'application/json'
-          
-          #APIを叩いて結果をresponseへ代入
-          response = Net::HTTP.start(uri.host, uri.port) do |http|
-              http.request(request)
-          end
-          #結果をjsonにパースして格納
-          result = JSON.parse(response.body)
-          #ハッシュ値のみを出力
-          puts result['hash']
+      #パラメータをセット
+      #urlで"+"は" "になるので"%2b"にエスケープしてから代入
+      param = argv[0].to_s.gsub("+","%2b") 
+      #urlを作成
+      uri = URI.parse('http://challenge-server.code-check.io/api/hash?q='+param)
+      
+      request = Net::HTTP::Get.new(uri.request_uri)
+      # Request headers
+      request['Content-Type'] = 'application/json'
+      
+      #APIを叩いて結果をresponseへ代入
+      response = Net::HTTP.start(uri.host, uri.port) do |http|
+          http.request(request)
       end
+      #結果をjsonにパースして格納
+      result = JSON.parse(response.body)
+      #ハッシュ値のみを出力
+      puts result['hash']
     rescue => e
       #標準エラーを出力
       p e.message
